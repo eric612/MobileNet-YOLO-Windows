80557f91b36c4884c006d28f40ff4d93aeb5dfe0
Ming-Yang Liu <eric612kimo@yahoo.com.tw>
2018-07-08 12:04:54 +0800

    fix buigs

--- include/caffe/common.hpp
+++ include/caffe/common.hpp
@@ -168,8 +168,8 @@ class Caffe {
   inline static void set_solver_rank(int val) { Get().solver_rank_ = val; }
   inline static bool multiprocess() { return Get().multiprocess_; }
   inline static void set_multiprocess(bool val) { Get().multiprocess_ = val; }
-  inline static bool root_solver() { return Get().root_solver_; }
-  inline static void set_root_solver(bool val) { Get().root_solver_ = val; }
+  inline static bool root_solver() { return Get().solver_rank_ == 0; }
+
  protected:
 #ifndef CPU_ONLY
   cublasHandle_t cublas_handle_;
@@ -178,10 +178,12 @@ class Caffe {
   shared_ptr<RNG> random_generator_;
 
   Brew mode_;
+
+  // Parallel training
   int solver_count_;
   int solver_rank_;
   bool multiprocess_;
-  bool root_solver_;
+
  private:
   // The private constructor to avoid duplicate instantiation.
   Caffe();
